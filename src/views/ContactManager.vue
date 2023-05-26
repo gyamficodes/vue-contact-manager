fbcafbbd0a821536a239696a7980a8a83fef7f70
gyamficodes <gyamfijohn08@gmail.com>
2023-05-26 10:35:24 -0700

    src page update

--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -1,18 +1,57 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
+<!-- eslint-disable vuejs-accessibility/form-control-has-label -->
 <!-- eslint-disable max-len -->
 <template>
   <div class="contacts">
     <div class="container">
       <div class="row">
-        <div class="col" >
-        <div id="page">
-          <p class="h3 mt-3 text-success fw-bold">Contact Manager</p>
-        <router-link to="/add">
-          <button  type="button" class="btn btn-success btn-sm mt-3"><i class="fa-solid fa-plus circle"></i> New</button>
-        </router-link>
-        </div>
+        <div class="col">
+          <div id="page">
+            <p class="h3 mt-3 text-success fw-bold">Contact Manager</p>
+            <router-link to="/add">
+              <button type="button" class="btn btn-success btn-sm mt-3">
+                <i class="fa-solid fa-plus circle"></i> New
+              </button>
+            </router-link>
+          </div>
+          <p class="fst-italic">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, modi? Similique,
+            provident? Vel sunt quo doloribus dolore assumenda ad excepturi. Quo magnam nostrum
+            nobis sequi eaque, quam tempora inventore expedita?
+          </p>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="row">
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Search Name" />
+                </div>
+                <div class="col">
+                  <input type="submit" class="btn btn-outline-dark inp" placeholder="submit" />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+   <div class="container mt-3">
+    <div class="row ">
+      <div class="col-md-6">
+        <div class="card my-2 cd">
+          <div class="card-body">
+          <div class="row">
+          <!-- image -->
+          <div class="col-sm-4">
+          <img class="Contact-image" src="https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png" alt="">
+          </div>
+          <div class="col-sm-7">dsvs</div>
+          <div class="col-sm-1">ascasc</div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </div>
   </div>
 </template>
 <script></script>
