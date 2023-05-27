3ca7ccfa4001645831b38a5cb416ce207b093ece
gyamficodes <gyamfijohn08@gmail.com>
2023-05-27 08:22:40 -0700

    update

--- src/views/AddContact.vue
+++ src/views/AddContact.vue
@@ -1,5 +1,22 @@
+<!-- eslint-disable max-len -->
 <template>
   <div class="add">
-    <h1>add</h1>
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col">
+          <h3 class="text-success">Add Contact</h3>
+          <p class="fontstyle-italic">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda eos error cum, nihil
+            repudiandae officia sit, amet voluptatibus vero deserunt soluta ut repellat ea dolorem
+            eveniet animi esse ab dignissimos.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-md-3"></div>
+      </div>
+    </div>
   </div>
 </template>
--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -34,24 +34,39 @@
         </div>
       </div>
     </div>
-   <div class="container mt-3">
-    <div class="row ">
-      <div class="col-md-6">
-        <div class="card my-2 cd">
-          <div class="card-body">
-          <div class="row">
-          <!-- image -->
-          <div class="col-sm-4">
-          <img class="Contact-image" src="https://assets.stickpng.com/images/585e4bcdcb11b227491c3396.png" alt="">
-          </div>
-          <div class="col-sm-7">dsvs</div>
-          <div class="col-sm-1">ascasc</div>
-          </div>
+    <div class="container mt-3">
+      <div class="row ">
+        <div class="col-md-6">
+          <div class="card my-2 cd shadow-lg">
+            <div class="card-body">
+              <div class="row align-items-center">
+                <!-- image -->
+                <div class="col-sm-4">
+                  <img class="Contact-image"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4CRKPij6o2waFROp-89BCE8lEf96jLsndRQ&usqp=CAU"
+                    alt="">
+                </div>
+                <!-- items -->
+                <div class="col-sm-7">
+                <ul class="list-group">
+                <li class="list-group-item">Name: <span class="fw-bold ">Name</span></li>
+                <li class="list-group-item"> Email: <span class="fw-bold ">Email</span></li>
+                <li class="list-group-item">Contact: <span class="fw-bold ">Contact</span></li>
+
+                </ul>
+                </div>
+                <!-- route -->
+                <div class="col-sm-1 box-f">
+                  <router-link to="/view" class="btn btn-warning my-1"><i class="fa-solid fa-eye"></i></router-link>
+                  <router-link to="/edit" class="btn btn-primary my-1"><i class="fa-solid fa-pen"></i></router-link>
+                  <button type="button" class="btn btn-danger my-1"><i class="fa-solid fa-trash"></i></button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-   </div>
   </div>
 </template>
 <script></script>
